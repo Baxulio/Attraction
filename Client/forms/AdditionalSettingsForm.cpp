d569e2ecd99158da1bda8b9fa271ab17a1078673
Bakhman <istbahman@mail.ru>
2018-07-04 01:16:29 +0500

    Signed-off-by: Bakhman <istbahman@mail.ru>

--- Client/forms/AdditionalSettingsForm.cpp
+++ Client/forms/AdditionalSettingsForm.cpp
@@ -1,6 +1,10 @@
 #include "AdditionalSettingsForm.h"
 #include "ui_AdditionalSettingsForm.h"
 
+#include <QMessageBox>
+
+#include "dialogs/WieagandReaderDialog.h"
+
 AdditionalSettingsForm::AdditionalSettingsForm(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::AdditionalSettingsForm),
@@ -9,6 +13,7 @@ AdditionalSettingsForm::AdditionalSettingsForm(QWidget *parent) :
     ui->setupUi(this);
 
     ui->tariffs_table->setModel(bDb.tariffModel);
+    ui->staff_table->setModel(&staffModel);
 
     connect(&bDb, &DatabaseManager::refresh, this, &AdditionalSettingsForm::on_refresh);
     on_refresh();
@@ -21,21 +26,31 @@ AdditionalSettingsForm::~AdditionalSettingsForm()
 
 void AdditionalSettingsForm::on_refresh()
 {
+    staffModel.setTable("staff");
+    if(!staffModel.select()){
+        bDb.debugError(staffModel.lastError());
+        return ;
+    }
+
+    ui->staff_table->hideColumn(staffModel.fieldIndex("id_code"));
+    staffModel.setHeaderData(staffModel.fieldIndex("name"), Qt::Horizontal, "Имя",Qt::DisplayRole);
+    staffModel.setHeaderData(staffModel.fieldIndex("occupation"), Qt::Horizontal, "Должность",Qt::DisplayRole);
+
     ui->tariffs_table->hideColumn(bDb.tariffModel->fieldIndex("id"));
 
-    bDb.tariffModel->setHeaderData(bDb.tariffModel->fieldIndex("title"),Qt::Horizontal,"Название");
-    bDb.tariffModel->setHeaderData(bDb.tariffModel->fieldIndex("price"),Qt::Horizontal,"Цена");
-    bDb.tariffModel->setHeaderData(bDb.tariffModel->fieldIndex("time_limit"),Qt::Horizontal,"Время [в минутах]");
+    bDb.tariffModel->setHeaderData(bDb.tariffModel->fieldIndex("title"),Qt::Horizontal,"Название",Qt::DisplayRole);
+    bDb.tariffModel->setHeaderData(bDb.tariffModel->fieldIndex("price"),Qt::Horizontal,"Цена",Qt::DisplayRole);
+    bDb.tariffModel->setHeaderData(bDb.tariffModel->fieldIndex("time_limit"),Qt::Horizontal,"Время [в минутах]",Qt::DisplayRole);
 }
 
 void AdditionalSettingsForm::on_add_tariff_clicked()
 {
-//    tariffs_model->insertRow(tariffs_model->rowCount());
+    //    tariffs_model->insertRow(tariffs_model->rowCount());
 }
 
 void AdditionalSettingsForm::on_delete_tariff_clicked()
 {
-//    tariffs_model->removeRow(ui->tariffs_table->currentIndex().row());
+    //    tariffs_model->removeRow(ui->tariffs_table->currentIndex().row());
 }
 
 void AdditionalSettingsForm::on_tariffs_submit_clicked()
@@ -52,4 +67,52 @@ void AdditionalSettingsForm::on_tariffs_revert_clicked()
 void AdditionalSettingsForm::on_back_but_clicked()
 {
     emit back();
+}
+
+void AdditionalSettingsForm::on_add_stuff_but_clicked()
+{
+    quint32 code;
+    if(!getCode(code))return;
+
+    int row = staffModel.rowCount();
+    staffModel.insertRow(row);
+    staffModel.setData(staffModel.index(row,staffModel.fieldIndex("id_code")),code);
+}
+
+void AdditionalSettingsForm::on_delete_stuff_but_clicked()
+{
+    staffModel.removeRow(ui->staff_table->currentIndex().row());
+}
+
+void AdditionalSettingsForm::on_staff_submit_clicked()
+{
+    if(!staffModel.submitAll())
+        bDb.debugError(staffModel.lastError());
+}
+
+void AdditionalSettingsForm::on_staff_revert_clicked()
+{
+    staffModel.revertAll();
+}
+
+void AdditionalSettingsForm::on_chang_bracer_but_clicked()
+{
+    quint32 code;
+    if(getCode(code))
+        staffModel.setData(staffModel.index(ui->staff_table->currentIndex().row(),staffModel.fieldIndex("id_code")),code);
+}
+
+bool AdditionalSettingsForm::getCode(quint32 &code)
+{
+    WieagandReaderDialog dialog(this);
+    if(dialog.exec() != QDialog::Accepted){
+        return false;
+    }
+    code=dialog.getCode();
+    if(!code){
+        QMessageBox::warning(this, "Неожиданная ситуация",
+                             QString("Неверный ввод!"));
+        return false;
+    }
+    return true;
 }
--- Client/forms/AdditionalSettingsForm.h
+++ Client/forms/AdditionalSettingsForm.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include "DatabaseManager.h"
+#include "models/CustomSqlTableModel.h"
 
 namespace Ui {
 class AdditionalSettingsForm;
@@ -20,19 +21,29 @@ private slots:
     void on_refresh();
 
     void on_add_tariff_clicked();
-
     void on_delete_tariff_clicked();
 
     void on_tariffs_submit_clicked();
-
     void on_tariffs_revert_clicked();
 
     void on_back_but_clicked();
+
+    void on_add_stuff_but_clicked();
+    void on_delete_stuff_but_clicked();
+
+    void on_staff_submit_clicked();
+    void on_staff_revert_clicked();
+
+    void on_chang_bracer_but_clicked();
 
 private:
     Ui::AdditionalSettingsForm *ui;
     DatabaseManager &bDb;
 
+    CustomSqlTableModel staffModel;
+
+private:
+    bool getCode(quint32 &code);
 signals:
     void back();
 };
